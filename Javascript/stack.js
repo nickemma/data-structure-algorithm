29a1315e957d06fc988250b82c38f85b4e9776d4
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2024-07-07 03:36:07 +0100

    Done

--- Javascript/stack.js
+++ Javascript/stack.js
@@ -1,5 +1,3 @@
-//  Create a stack data structure. The stack should be a class with methods 'push', 'pop', and 'peek'. Adding an element to the stack should store it until it is removed.
-
 class Stack {
   // Array is used to implement stack
   constructor() {
